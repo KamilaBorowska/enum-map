267f9dd21e700f5c3662ead537fd906a777f2707
Konrad Borowski <xfix@protonmail.com>
2017-05-16 22:30:21 +0200

    Initial commit of enum-map

---
+++ enum-map-derive/src/lib.rs
@@ -0,0 +1,74 @@
+#![recursion_limit = "128"]
+
+extern crate proc_macro;
+extern crate syn;
+#[macro_use]
+extern crate quote;
+
+use std::iter;
+
+use proc_macro::TokenStream;
+use syn::{Body, Ident, Variant, VariantData};
+use quote::Tokens;
+
+fn generate_enum_code(name: &Ident, variants: &[Variant]) -> Tokens {
+    let mut enum_count = 0usize;
+    for &Variant {
+             ref data,
+             ref discriminant,
+             ..
+         } in variants {
+        if data != &VariantData::Unit {
+            panic!("#[derive(EnumMap)] requires C style style enum");
+        }
+        if discriminant.is_some() {
+            panic!("#[derive(EnumMap)] doesn't currently support discriminants");
+        }
+        enum_count += 1;
+    }
+
+    let variant_a = variants.iter().map(|variant| &variant.ident);
+    let variant_b = variants.iter().map(|variant| &variant.ident);
+    let repeat_name_a = iter::repeat(name).take(variants.len());
+    let repeat_name_b = repeat_name_a.clone();
+    let counter = 0..variants.len();
+
+    quote! {
+        impl<V> ::enum_map::Internal<V> for #name {
+            type Array = [V; #enum_count];
+            fn slice(array: &Self::Array) -> &[V] {
+                array
+            }
+            fn slice_mut(array: &mut Self::Array) -> &mut [V] {
+                array
+            }
+            fn from_usize(value: usize) -> Self {
+                match value {
+                    #(
+                        #counter => #repeat_name_a::#variant_a,
+                    )*
+                    _ => unreachable!()
+                }
+            }
+            fn to_usize(self) -> usize {
+                self as usize
+            }
+            fn from_function<F: Fn(Self) -> V>(f: F) -> Self::Array {
+                [#(
+                    f(#repeat_name_b::#variant_b),
+                )*]
+            }
+        }
+    }
+}
+
+#[proc_macro_derive(EnumMap)]
+pub fn derive_enum_map(input: TokenStream) -> TokenStream {
+    let input = syn::parse_macro_input(&input.to_string()).unwrap();
+    match input.body {
+            Body::Enum(ref variants) => generate_enum_code(&input.ident, variants),
+            _ => panic!("#[derive(EnumMap)] is only defined for enums"),
+        }
+        .parse()
+        .unwrap()
+}
---
+++ enum-map/src/lib.rs
@@ -0,0 +1,250 @@
+//! An enum mapping type.
+//!
+//! It is implemented using array, so using it is as fast as using Rust
+//! arrays.
+
+#![no_std]
+#![deny(missing_docs)]
+
+use core::hash::{Hash, Hasher};
+use core::iter::Enumerate;
+use core::marker::PhantomData;
+use core::ops::{Index, IndexMut};
+use core::slice;
+
+// this is not stable, do not depend on this
+#[doc(hidden)]
+pub trait Internal<V>: Sized {
+    type Array;
+    fn slice(&Self::Array) -> &[V];
+    fn slice_mut(&mut Self::Array) -> &mut [V];
+    fn from_usize(usize) -> Self;
+    fn to_usize(self) -> usize;
+    fn from_function<F: Fn(Self) -> V>(F) -> Self::Array;
+}
+
+/// An enum mapping.
+///
+/// This internally uses an array which stores a value for each possible
+/// enum value. To work, it requires implementation of internal (private,
+/// although public due to macro limitations) trait which allows extracting
+/// information about an enum, which can be automatically generated using
+/// `#[derive(EnumMap)]` from `enum_map_derive` crate.
+///
+/// # Examples
+///
+/// ```
+/// extern crate enum_map;
+/// #[macro_use]
+/// extern crate enum_map_derive;
+///
+/// use enum_map::EnumMap;
+///
+/// #[derive(EnumMap)]
+/// enum Example {
+///     A,
+///     B,
+///     C,
+/// }
+///
+/// fn main() {
+///     let mut map = EnumMap::new();
+///     // new initializes map with default values
+///     assert_eq!(map[Example::A], 0);
+///     map[Example::A] = 3;
+///     assert_eq!(map[Example::A], 3);
+/// }
+/// ```
+pub struct EnumMap<K: Internal<V>, V> {
+    array: K::Array,
+}
+
+impl<K: Internal<V>, V> EnumMap<K, V> {
+    /// Returns an iterator over enum map.
+    pub fn iter(&self) -> Iter<K, V> {
+        self.into_iter()
+    }
+}
+
+impl<F: Fn(K) -> V, K: Internal<V>, V> From<F> for EnumMap<K, V> {
+    fn from(f: F) -> Self {
+        EnumMap { array: K::from_function(f) }
+    }
+}
+
+/// Enum map constructor.
+///
+/// This macro allows to create a new enum map in a type safe way. It takes
+/// a list of `,` separated pairs separated by `=>`. Left side is `|`
+/// separated list of enum keys, or `_` to match all unmatched enum keys,
+/// while right side is a value.
+///
+/// # Examples
+///
+/// ```
+/// #[macro_use]
+/// extern crate enum_map;
+/// #[macro_use]
+/// extern crate enum_map_derive;
+///
+/// #[derive(EnumMap)]
+/// enum Example {
+///     A,
+///     B,
+///     C,
+///     D,
+/// }
+///
+/// fn main() {
+///     let enum_map = enum_map! {
+///         Example::A | Example::B => 1,
+///         Example::C => 2,
+///         _ => 3,
+///     };
+///     assert_eq!(enum_map[Example::A], 1);
+///     assert_eq!(enum_map[Example::B], 1);
+///     assert_eq!(enum_map[Example::C], 2);
+///     assert_eq!(enum_map[Example::D], 3);
+/// }
+/// ```
+#[macro_export]
+macro_rules! enum_map {
+    {$($t:tt)*} => {
+        ::enum_map::EnumMap::from(|k| match k { $($t)* })
+    };
+}
+
+impl<K: Internal<V>, V: Default> EnumMap<K, V>
+    where K::Array: Default
+{
+    /// Creates an enum map with default values.
+    ///
+    /// ```
+    /// extern crate enum_map;
+    /// #[macro_use]
+    /// extern crate enum_map_derive;
+    ///
+    /// use enum_map::EnumMap;
+    ///
+    /// #[derive(EnumMap)]
+    /// enum Example {
+    ///     A,
+    /// }
+    ///
+    /// fn main() {
+    ///     let enum_map = EnumMap::<_, i32>::new();
+    ///     assert_eq!(enum_map[Example::A], 0);
+    /// }
+    /// ```
+    pub fn new() -> Self {
+        EnumMap::default()
+    }
+}
+
+// Implementations provided by derive attribute are too specific, and put requirements on K.
+// This is caused by rust-lang/rust#26925.
+impl<K: Internal<V>, V> Clone for EnumMap<K, V>
+    where K::Array: Clone
+{
+    fn clone(&self) -> Self {
+        EnumMap { array: self.array.clone() }
+    }
+}
+
+impl<K: Internal<V>, V> Copy for EnumMap<K, V> where K::Array: Copy {}
+
+impl<K: Internal<V>, V> PartialEq for EnumMap<K, V>
+    where K::Array: PartialEq
+{
+    fn eq(&self, other: &Self) -> bool {
+        self.array == other.array
+    }
+}
+
+impl<K: Internal<V>, V> Eq for EnumMap<K, V> where K::Array: Eq {}
+
+impl<K: Internal<V>, V> Hash for EnumMap<K, V>
+    where K::Array: Hash
+{
+    fn hash<H: Hasher>(&self, state: &mut H) {
+        self.array.hash(state);
+    }
+}
+
+impl<K: Internal<V>, V> Default for EnumMap<K, V>
+    where K::Array: Default
+{
+    fn default() -> Self {
+        EnumMap { array: K::Array::default() }
+    }
+}
+
+impl<K: Internal<V>, V> Index<K> for EnumMap<K, V> {
+    type Output = V;
+
+    fn index(&self, key: K) -> &V {
+        &K::slice(&self.array)[key.to_usize()]
+    }
+}
+
+impl<K: Internal<V>, V> IndexMut<K> for EnumMap<K, V> {
+    fn index_mut(&mut self, key: K) -> &mut V {
+        &mut K::slice_mut(&mut self.array)[key.to_usize()]
+    }
+}
+
+/// Immutable enum map iterator
+///
+/// This struct is created by `iter` method or `into_iter` on a reference
+/// to `EnumMap`.
+///
+/// # Examples
+///
+/// ```
+/// #[macro_use]
+/// extern crate enum_map;
+/// #[macro_use]
+/// extern crate enum_map_derive;
+///
+/// #[derive(EnumMap)]
+/// enum Example {
+///     A,
+///     B,
+///     C,
+/// }
+///
+/// fn main() {
+///     let mut map = enum_map! { Example::A => 3, _ => 0 };
+///     assert_eq!(map[Example::A], 3);
+///     for (key, &value) in &map {
+///         assert_eq!(match key {
+///             Example::A => 3,
+///             _ => 0,
+///         }, value);
+///     }
+/// }
+/// ```
+pub struct Iter<'a, K, V: 'a> {
+    _phantom: PhantomData<K>,
+    iterator: Enumerate<slice::Iter<'a, V>>,
+}
+
+impl<'a, K: Internal<V>, V> Iterator for Iter<'a, K, V> {
+    type Item = (K, &'a V);
+    fn next(&mut self) -> Option<Self::Item> {
+        self.iterator
+            .next()
+            .map(|(index, item)| (K::from_usize(index), item))
+    }
+}
+
+impl<'a, K: Internal<V>, V> IntoIterator for &'a EnumMap<K, V> {
+    type Item = (K, &'a V);
+    type IntoIter = Iter<'a, K, V>;
+    fn into_iter(self) -> Self::IntoIter {
+        Iter {
+            _phantom: PhantomData,
+            iterator: K::slice(&self.array).iter().enumerate(),
+        }
+    }
+}
