cfb36df1e80177a02dfce52b15022268087cf467
Konrad Borowski <konrad@borowski.pw>
2017-08-29 22:13:10 +0200

    Check if enum_map! can be used on variantless enum

---
+++ enum-map/tests/macro.rs
@@ -0,0 +1,15 @@
+#![no_std]
+
+#[macro_use]
+extern crate enum_map;
+
+use enum_map::EnumMap;
+
+#[derive(EnumMap)]
+enum Void {}
+
+#[test]
+fn empty_map() {
+    let void: EnumMap<Void, Void> = enum_map! {};
+    assert!(void.is_empty());
+}
