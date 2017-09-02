18f64c75e22b0ad85f1224ca50bc48ad695ed48b
Konrad Borowski <konrad@borowski.pw>
2017-09-02 22:21:22 +0200

    Borrow options in visit_map instead of moving them

--- enum-map/src/serde.rs
+++ enum-map/src/serde.rs
@@ -56,7 +56,7 @@ where
             entries[key] = Some(value);
         }
         for value in entries.values() {
-            value.ok_or_else(|| M::Error::custom("key not specified"))?;
+            value.as_ref().ok_or_else(|| M::Error::custom("key not specified"))?;
         }
         Ok(enum_map! { key => entries[key].take().unwrap() })
     }
