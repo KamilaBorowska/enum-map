28200bd8143f139cd6fda5027bd720931afb0966
Konrad Borowski <github@borowski.pw>
2018-04-20 15:02:30 +0200

    Test map cloning

--- enum-map/tests/test.rs
+++ enum-map/tests/test.rs
@@ -48,6 +48,12 @@ fn test_option_bool() {
 }
 
 #[test]
+fn test_clone() {
+    let map = enum_map! { false => 3, true => 5 };
+    assert_eq!(map.clone(), map);
+}
+
+#[test]
 fn test_debug() {
     let map = enum_map! { false => 3, true => 5 };
     assert_eq!(format!("{:?}", map), "{false: 3, true: 5}");
