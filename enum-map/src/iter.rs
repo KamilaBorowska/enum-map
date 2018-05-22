10e6dfadfe615570fdbe4c9a0043642e453e8960
Konrad Borowski <github@borowski.pw>
2018-05-22 07:41:19 +0200

    Revert "Revert "Implement FusedIterator for enum map iterators""
    This reverts commit d9c6d24cf0d01e70ba684fea37167d4d0eff5239.

--- enum-map/src/iter.rs
+++ enum-map/src/iter.rs
@@ -1,6 +1,6 @@
 use {Enum, EnumMap};
 
-use core::iter::Enumerate;
+use core::iter::{Enumerate, FusedIterator};
 use core::marker::PhantomData;
 use core::mem::ManuallyDrop;
 use core::ptr;
@@ -63,6 +63,8 @@ impl<'a, K: Enum<V>, V> DoubleEndedIterator for Iter<'a, K, V> {
 }
 
 impl<'a, K: Enum<V>, V> ExactSizeIterator for Iter<'a, K, V> {}
+
+impl<'a, K: Enum<V>, V> FusedIterator for Iter<'a, K, V> {}
 
 impl<'a, K: Enum<V>, V> IntoIterator for &'a EnumMap<K, V> {
     type Item = (K, &'a V);
@@ -130,6 +132,8 @@ impl<'a, K: Enum<V>, V> DoubleEndedIterator for IterMut<'a, K, V> {
 
 impl<'a, K: Enum<V>, V> ExactSizeIterator for IterMut<'a, K, V> {}
 
+impl<'a, K: Enum<V>, V> FusedIterator for IterMut<'a, K, V> {}
+
 impl<'a, K: Enum<V>, V> IntoIterator for &'a mut EnumMap<K, V> {
     type Item = (K, &'a mut V);
     type IntoIter = IterMut<'a, K, V>;
@@ -191,6 +195,8 @@ impl<K: Enum<V>, V> Iterator for IntoIter<K, V> {
 }
 
 impl<K: Enum<V>, V> ExactSizeIterator for IntoIter<K, V> {}
+
+impl<K: Enum<V>, V> FusedIterator for IntoIter<K, V> {}
 
 impl<K: Enum<V>, V> Drop for IntoIter<K, V> {
     fn drop(&mut self) {
