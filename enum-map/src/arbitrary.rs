cd19cbe3318f153ebecad529d957cdb0a7114782
Konrad Borowski <konrad@borowski.pw>
2021-05-04 16:22:36 +0200

    Implement Arbitrary::size_hint for EnumMap

--- enum-map/src/arbitrary.rs
+++ enum-map/src/arbitrary.rs
@@ -1,3 +1,4 @@
+use crate::internal::Array;
 use crate::{enum_map, Enum, EnumMap};
 use arbitrary::{Arbitrary, Result, Unstructured};
 
@@ -6,5 +7,17 @@ impl<'a, K: Enum<V>, V: Arbitrary<'a>> Arbitrary<'a> for EnumMap<K, V> {
         Ok(enum_map! {
             _ => Arbitrary::arbitrary(u)?,
         })
+    }
+
+    fn size_hint(depth: usize) -> (usize, Option<usize>) {
+        if K::Array::LENGTH == 0 {
+            (0, Some(0))
+        } else {
+            let (lo, hi) = V::size_hint(depth);
+            (
+                lo.saturating_mul(K::Array::LENGTH),
+                hi.and_then(|hi| hi.checked_mul(K::Array::LENGTH)),
+            )
+        }
     }
 }
