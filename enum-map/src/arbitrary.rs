613d4f937598cedecb2242a22c6d81355902ec11
Konrad Borowski <konrad@borowski.pw>
2021-05-04 16:23:06 +0200

    Document that Arbitrary implementation requires crate feature

--- enum-map/src/arbitrary.rs
+++ enum-map/src/arbitrary.rs
@@ -2,6 +2,7 @@ use crate::internal::Array;
 use crate::{enum_map, Enum, EnumMap};
 use arbitrary::{Arbitrary, Result, Unstructured};
 
+/// Requires crate feature `"serde"`
 impl<'a, K: Enum<V>, V: Arbitrary<'a>> Arbitrary<'a> for EnumMap<K, V> {
     fn arbitrary(u: &mut Unstructured<'a>) -> Result<EnumMap<K, V>> {
         Ok(enum_map! {
